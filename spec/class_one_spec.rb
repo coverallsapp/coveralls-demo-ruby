a777dd471ce45a7ff0c6d9f68da84d84b5993ad9
James Kessler <afinetooth@gmail.com>
2024-01-31 13:29:08 -0800

    Make another change irrelevant to coverage to test build status for drifted build with new setting.

--- spec/class_one_spec.rb
+++ spec/class_one_spec.rb
@@ -3,6 +3,7 @@ require 'class_one'
 
 describe ClassOne do
 
+  # One more change (comment) irrelevant to coverage
   describe "covered" do
     it "returns 'covered'" do
       expect(ClassOne.covered).to eql("covered")
