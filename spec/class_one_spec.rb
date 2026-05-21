b728e07f21f01824b7273d113ad47dacd8449394
James Kessler <afinetooth@gmail.com>
2026-05-20 17:49:20 -0700

    Un-comment all tests to increase coverage to the max, while testing that patch sttaus is turned OFF.

--- spec/class_one_spec.rb
+++ spec/class_one_spec.rb
@@ -12,11 +12,11 @@ describe ClassOne do
   end
 
   # Uncomment below to achieve 100% coverage
-  # describe "uncovered" do
-  #   it "returns 'uncovered'" do
-  #     expect(ClassOne.uncovered).to eql("uncovered")
-  #   end
-  # end
+  describe "uncovered" do
+    it "returns 'uncovered'" do
+      expect(ClassOne.uncovered).to eql("uncovered")
+    end
+  end
 
   # Uncomment below to achieve 100% coverage
   describe "delta" do
